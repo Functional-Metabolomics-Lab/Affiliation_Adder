b6c45d96e724bef2000c9edeeade2ca5c23d6bb6
Abzer <abzer.kelmina@gmail.com>
2023-10-16 14:11:35 +0200

    File added

---
+++ affil_adder2.py
@@ -0,0 +1,71 @@
+# Import necessary libraries
+import streamlit as st
+import pandas as pd
+from io import StringIO
+import base64
+
+# Superscript mapping for numbers
+SUPERSCRIPT_MAP = {
+    '0': '⁰',
+    '1': '¹',
+    '2': '²',
+    '3': '³',
+    '4': '⁴',
+    '5': '⁵',
+    '6': '⁶',
+    '7': '⁷',
+    '8': '⁸',
+    '9': '⁹'
+}
+
+def to_superscript(number):
+    """Convert a number to its superscript representation."""
+    return ''.join([SUPERSCRIPT_MAP[digit] for digit in str(number)])
+
+# Function to download the results as a txt file
+def download_link(content, filename, text):
+    buffer = StringIO()
+    buffer.write(content)
+    buffer.seek(0)
+    b64 = base64.b64encode(buffer.read().encode()).decode()
+    return f'<a href="data:application/octet-stream;base64,{b64}" download="{filename}">{text}</a>'
+
+def main():
+    st.title("AffilAdder: Author Affiliation Linker")
+
+    uploaded_file = st.file_uploader("Upload your Excel file", type=['xlsx'])
+
+    if uploaded_file is not None:
+        # Read the Excel file
+        df = pd.read_excel(uploaded_file)
+
+        # Collect unique affiliations in order
+        affiliations = []
+        for _, row in df.iterrows():
+            for column in ["Affiliation1", "Affiliation2", "Affiliation3"]:
+                if pd.notna(row[column]) and row[column] not in affiliations:
+                    affiliations.append(row[column])
+                    
+        # Associate authors with affiliation indices
+        author_strings = []
+        for _, row in df.iterrows():
+            affil_indices = [affiliations.index(row[column]) + 1 for column in ["Affiliation1", "Affiliation2", "Affiliation3"] if pd.notna(row[column])]
+            affiliation_string = ','.join([to_superscript(i) for i in affil_indices])
+            author_string = f"{row['Author']}{affiliation_string}"
+            author_strings.append(author_string)
+            
+        # Combine author strings with commas
+        authors_text = ', '.join(author_strings)
+        
+        # List affiliations at the end
+        affiliation_strings = [f"{i+1}. {affiliation}" for i, affiliation in enumerate(affiliations)]
+        
+        # Compile the entire content
+        combined_text = authors_text + '\n\n' + '\n'.join(affiliation_strings)
+        
+        # Display and provide download link
+        st.text_area("Processed Author Affiliations", combined_text, height=400)
+        st.markdown(download_link(combined_text, 'author_affiliations.txt', 'Click here to download the text file'), unsafe_allow_html=True)
+
+if __name__ == "__main__":
+    main()
